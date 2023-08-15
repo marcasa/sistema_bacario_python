a0ca357a7cff85f107c76c915cbf895d6c2edb39
MarceloC <marcelo.casanova@live.com>
2023-08-15 10:56:55 -0300

    Concluída a função "Saque" e "Extrato"

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,3 +1,6 @@
+from datetime import date
+
+
 menu = """
 ========SISTEMA BANCÁRIO============
 Digite a opção desejada:
@@ -10,27 +13,30 @@ Digite a opção desejada:
 
 """
 extrato = """
-==========EXTRATO===========
+===============EXTRATO==============
+OPERAÇÃO ----- DATA ----- VALOR 
 """
 saldo = 5000.00
 qtd_saques = 0
 LIMITE_SAQUE_DIARIO = 500.00
-
 saque = 0.00
+data_hoje = date.today()
 
 while True :
     
     opcao = input(menu)
 
     if opcao == "1" :
+        extrato += f"\n SALDO EM {data_hoje.day}/{data_hoje.month}/{data_hoje.year} R$ {saldo}"
         print(extrato)
     elif opcao == "2" :
         saque = float(input("Valor a ser sacado: "))
-        if qtd_saques <= 3 and saque <= 500.00 and saque <= saldo :
+        if qtd_saques < 3 and saque <= 500.00 and saque <= saldo :
             saldo -= saque
             qtd_saques += 1 
             print("Saque efetuado com sucesso, retire seu dinheiro")
             print(f"Saldo atual = {saldo}")
+            extrato += f"\n SAQUE ----- {data_hoje.day}/{data_hoje.month}/{data_hoje.year} ----- R$ {saque:.2f}"
         else :
             print("Erro ao efetuar o saque...")
             
